2a68cb7642d58cd87a1aa4ff33b3c81028d87a2f
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-10-19 11:51:06 +0530

    server setup successfully

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("hello server");
+// Import the Express library
+import express from "express";
+
+// Create an instance of the Express application
+const app = express();
+app.use(express.json());
+
+// health
+app.get("/health", (req, res) => {
+  res.json({
+    success: true,
+    message: "All set ",
+  });
+});
+
+// Set the port number for the server
+const PORT = 4000;
+
+// Start the server and listen on the specified port
+app.listen(PORT, () => {
+  console.log(`Starting Express server on port ${PORT}`);
+});
